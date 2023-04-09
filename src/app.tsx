d13119a721bd30969ca2f8f3675cc934658c5672
Dmitry Manannikov <email@slonoed.net>
2023-04-09 15:32:02 -0700

    Fix types

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,11 @@
 import { Client as Styletron } from 'styletron-engine-atomic'
 import { Provider as StyletronProvider } from 'styletron-react'
 import { LightTheme, BaseProvider } from 'baseui'
-import CodeScanner from './code-scanner'
 import Drinks from './drinks'
 
 const engine = new Styletron()
 
 function App() {
-  const onNewScanResult = (...args) => {
-    console.log(args)
-  }
   return (
     <StyletronProvider value={engine}>
       <BaseProvider theme={LightTheme}>
